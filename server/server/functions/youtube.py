ff16712b13c5f542cc1b1d9706c0370d56c57b5a
poteitoo <1105tigery@gmail.com>
2024-03-05 19:38:12 +0000

    bugfix regect live streaming videos

--- server/server/functions/youtube.py
+++ server/server/functions/youtube.py
@@ -10,11 +10,11 @@ def get_youtube_video_info(video_url: str):
             raise ValueError("Not found")
 
 
-def shorter_than_one_hour(info, *, incomplete):
-    """Download only videos longer than a minute (or with unknown duration)"""
+def checkIfIsAvailableVideoA(info, _, __):
     duration = info.get("duration")
-    print("duration: ", duration)
-    if duration and duration > 120 * 60:  # 120 minutes
+    if duration is None:  # ライブストリームなどdurationがわからない場合
+        raise SystemError("Video duration is unknown")
+    if duration > 120 * 60:  # 120 minutes までの動画を取得可能
         raise SystemError("Video longer than 10 minutes")
 
 
@@ -22,7 +22,7 @@ ydl_opts = {
     "extract_audio": True,
     "format": "bestaudio",
     "outtmpl": "audios/%(id)s.%(ext)s",
-    "match_filter": shorter_than_one_hour,
+    "match_filter": checkIfIsAvailableVideoA,
 }
 
 
