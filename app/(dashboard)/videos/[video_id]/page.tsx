78df1950903d6cf2fffb50d5ff36aab0128c9ca0
poteitoo <1105tigery@gmail.com>
2024-03-08 09:13:17 +0000

    add video pages

---
+++ app/(dashboard)/videos/[video_id]/page.tsx
@@ -0,0 +1,91 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useFullscreen } from "@/hooks/use-fullscreen";
+import { getVideoById } from "@/repositories/video";
+import { useParams } from "next/navigation";
+import { useEffect, useRef, useState } from "react";
+import YouTube from "react-youtube";
+import useSWRImmutable from "swr/immutable";
+
+export default function Page() {
+  const [currentTime, setCurrentTime] = useState(0);
+  const youtubeRef = useRef<YT.Player | null>(null);
+  const { video_id } = useParams<{ video_id: string }>();
+  const { data: video } = useSWRImmutable(["videos", video_id], () =>
+    getVideoById(video_id),
+  );
+  const { isFullscreen, toggle: handleFullscreenChange } = useFullscreen();
+
+  const handleYoutubeReady = (event: YT.PlayerEvent) => {
+    youtubeRef.current = event.target;
+  };
+  const seekTo = (start?: string) => {
+    const parsed = parseFloat(start || "");
+    if (youtubeRef.current) {
+      youtubeRef.current?.seekTo(parsed, true);
+    }
+  };
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      const curr = youtubeRef.current?.getCurrentTime();
+      setCurrentTime(curr ?? 0);
+    }, 500);
+    return () => clearInterval(intervalId);
+  }, []);
+
+  if (!video)
+    return (
+      <div className="flex h-full w-full flex-col items-center justify-center gap-3">
+        <div>Loading...</div>
+      </div>
+    );
+
+  return (
+    <div className="relative flex h-full w-full flex-col items-center gap-3">
+      <div className="fixed grid h-full w-full grid-cols-2 gap-3 p-3">
+        <div className="h-full w-full">
+          <YouTube
+            videoId={video_id}
+            className="h-full w-full"
+            iframeClassName="h-full w-full"
+            onReady={handleYoutubeReady}
+          />
+        </div>
+        <div
+          data-fullscreen={isFullscreen}
+          className="row-span-2 h-[calc(100vh-64px)] w-full overflow-auto pb-32 data-[fullscreen=false]:h-[calc(100vh-100px)]"
+        >
+          <div className="flex flex-col gap-5">
+            {video.segments?.map((segment, i) => (
+              <div key={segment.id} className="">
+                <Button
+                  variant="link"
+                  data-start={segment.start}
+                  data-end={segment.end}
+                  onClick={() => seekTo(segment.start)}
+                >
+                  {segment.speaker}
+                </Button>
+                <section className="flex gap-3">
+                  <p
+                    className="prose flex-1"
+                    data-start={segment.start}
+                    data-end={segment.end}
+                  >
+                    {segment.text}
+                  </p>
+                </section>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="h-full w-full">
+          <Button className="btn" onClick={handleFullscreenChange}>
+            {isFullscreen ? "exitFullscreen" : "fullscreen"}
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+}
--- app/(dashboard)/videos/page.tsx
+++ app/(dashboard)/videos/page.tsx
@@ -1,8 +1,35 @@
+"use client";
+
+import { getVideos } from "@/repositories/video";
+import Image from "next/image";
+import Link from "next/link";
+import useSWR from "swr";
+
 export default function Home() {
+  const { data: videos } = useSWR("videos/", () => getVideos());
+
   return (
-    <div className="flex h-full w-full flex-col items-center justify-between">
-      <div className="flex h-full w-full items-center justify-center">
-        動画一覧ページ
+    <div className="flex h-full w-full flex-col items-center gap-3">
+      <div className="mt-5 grid grid-cols-2 gap-1 gap-y-5 md:gap-3 lg:grid-cols-3">
+        {videos?.map(({ thumbnail, video_id, ...video }) =>
+          thumbnail ? (
+            <Link
+              href={`/videos/${video_id}`}
+              key={video.id}
+              className="w-full max-w-96 bg-base-100"
+            >
+              <Image
+                loader={() => thumbnail}
+                src="next.svg"
+                alt={video.title || "thumbnail"}
+                width={300}
+                height={200}
+                className="w-full rounded-md"
+              />
+              <h2 className="font-medium">{video.title}</h2>
+            </Link>
+          ) : null,
+        )}
       </div>
     </div>
   );
